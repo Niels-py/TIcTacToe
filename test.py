a5bce7e388b280b8c0cac65bb420ad19cc28ca96
Niels-py <65970112+Niels-py@users.noreply.github.com>
2021-01-30 14:51:15 +0100

    add of "play again"

--- test.py
+++ test.py
@@ -1,49 +1,50 @@
 # some variables
 
-list = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board_var: list = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 player = "X"
 run = True
+main_run: bool = True
 
 
 # printing the field
 def field():
-    print(f"{list[0]} | {list[1]} | {list[2]}")
+    print(f"{board_var[0]} | {board_var[1]} | {board_var[2]}")
     print("---------")
-    print(f"{list[3]} | {list[4]} | {list[5]}")
+    print(f"{board_var[3]} | {board_var[4]} | {board_var[5]}")
     print("---------")
-    print(f"{list[6]} | {list[7]} | {list[8]}")
+    print(f"{board_var[6]} | {board_var[7]} | {board_var[8]}")
 
 
 # convert input
 def eingabe():
     while True:
         x = input("Which position do you want to set on? ")
-        if x == "1" and list[0] == " ":
-            list[0] = player
+        if x == "1" and board_var[0] == " ":
+            board_var[0] = player
             break
-        elif x == "2" and list[1] == " ":
-            list[1] = player
+        elif x == "2" and board_var[1] == " ":
+            board_var[1] = player
             break
-        elif x == "3" and list[2] == " ":
-            list[2] = player
+        elif x == "3" and board_var[2] == " ":
+            board_var[2] = player
             break
-        elif x == "4" and list[3] == " ":
-            list[3] = player
+        elif x == "4" and board_var[3] == " ":
+            board_var[3] = player
             break
-        elif x == "5" and list[4] == " ":
-            list[4] = player
+        elif x == "5" and board_var[4] == " ":
+            board_var[4] = player
             break
-        elif x == "6" and list[5] == " ":
-            list[5] = player
+        elif x == "6" and board_var[5] == " ":
+            board_var[5] = player
             break
-        elif x == "7" and list[6] == " ":
-            list[6] = player
+        elif x == "7" and board_var[6] == " ":
+            board_var[6] = player
             break
-        elif x == "8" and list[7] == " ":
-            list[7] = player
+        elif x == "8" and board_var[7] == " ":
+            board_var[7] = player
             break
-        elif x == "9" and list[8] == " ":
-            list[8] = player
+        elif x == "9" and board_var[8] == " ":
+            board_var[8] = player
             break
         else:
             print("1 | 2 | 3")
@@ -55,39 +56,76 @@ def eingabe():
 
 # is there a winner already
 def winner():
-    if list[0] == list[1] == list[2] == "X" or list[0] == list[1] == list[2] == "O":
+    if board_var[0] == board_var[1] == board_var[2] == "X" or board_var[0] == board_var[1] == board_var[2] == "O":
         return False
-    elif list[3] == list[4] == list[5] == "X" or list[3] == list[4] == list[5] == "O":
+    elif board_var[3] == board_var[4] == board_var[5] == "X" or board_var[3] == board_var[4] == board_var[5] == "O":
         return False
-    elif list[6] == list[7] == list[8] == "X" or list[6] == list[7] == list[8] == "O":
+    elif board_var[6] == board_var[7] == board_var[8] == "X" or board_var[6] == board_var[7] == board_var[8] == "O":
         return False
-    elif list[0] == list[3] == list[6] == "X" or list[0] == list[3] == list[6] == "O":
+    elif board_var[0] == board_var[3] == board_var[6] == "X" or board_var[0] == board_var[3] == board_var[6] == "O":
         return False
-    elif list[1] == list[4] == list[7] == "X" or list[1] == list[4] == list[7] == "O":
+    elif board_var[1] == board_var[4] == board_var[7] == "X" or board_var[1] == board_var[4] == board_var[7] == "O":
         return False
-    elif list[2] == list[5] == list[8] == "X" or list[2] == list[5] == list[8] == "O":
+    elif board_var[2] == board_var[5] == board_var[8] == "X" or board_var[2] == board_var[5] == board_var[8] == "O":
         return False
-    elif list[0] == list[4] == list[8] == "X" or list[0] == list[4] == list[8] == "O":
+    elif board_var[0] == board_var[4] == board_var[8] == "X" or board_var[0] == board_var[4] == board_var[8] == "O":
         return False
-    elif list[2] == list[4] == list[6] == "X" or list[2] == list[4] == list[6] == "O":
+    elif board_var[2] == board_var[4] == board_var[6] == "X" or board_var[2] == board_var[4] == board_var[6] == "O":
         return False
     else:
         return True
 
 
+# test whether the board is full
+def board_full():
+    if board_var[0] != " " and board_var[1] != " " and board_var[2] != " " and board_var[3] != " " and board_var[
+        4] != " " and board_var[5] != " " and \
+            board_var[6] != " " and board_var[7] != " " and board_var[8] != " ":
+        print("draw!")
+        x = input("write \"play\" to restart: ")
+        if x != "play":
+            print("Bye")
+            run = False
+        else:
+
+            field()
+
+
 # main program
-while run:
+
+
+while main_run:
     field()
-    eingabe()
+    while run:
+
+        eingabe()
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+        field()
+        run = winner()
+        if run:
+            board_full()
+
     if player == "X":
         player = "O"
     else:
         player = "X"
-    run = winner()
 
-if player == "X":
-    player = "O"
-else:
-    player = "X"
+    board_var = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-print("Winner is player ", player)
+    print("Winner is player ", player)
+
+    while True:
+        again = input("Wanna play again?")
+        if again == "no":
+            print("Bye")
+            main_run = False
+            break
+        elif again == "yes":
+            print("Lets go for another round :)")
+            run = True
+            break
+        else:
+            print("yes = play again\nno  = dont play again")
